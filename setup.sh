fe23b7bd1416b9b5acade11a25bb6d50aacfe00b
MichaelGier <michael@4mgier.de>
2019-10-31 18:33:21 +0100

    3

--- setup.sh
+++ setup.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-echo "Kamera wird eingeschaltet"
+echo "*******************************"
+echo "*  Kamera wird eingeschaltet  *"
+echo "*******************************"
 # 0=on, 1=off
 sudo raspi-config nonint do_camera 0
 
-echo "System wird upgedatet"
+echo "***************************"
+echo "*  System wird upgedatet  *"
+echo "***************************"
 sudo apt update
 yes | sudo apt dist-upgrade
 
-echo "Abhängigkeiten werden installiert"
+echo "***************************************"
+echo "*  Abhängigkeiten werden installiert  *"
+echo "***************************************"
 yes | sudo apt install apache2 php7.1 php.7.1-gd ffmpeg git
 
-echo "Installation der FotoKiste"
+echo "********************************"
+echo "*  Installation der FotoKiste  *"
+echo "********************************"
 git clone https://github.com/MichaelGier/FotoKiste.git /home/pi/fotokiste
 
+echo "*****************************"
+echo "*  Konfiguration AutoStart  *"
+echo "*****************************"
+mkdir -p ~/.config/openbox
+echo "xset s noblank" >> ~/.config/openbox/autostart
+echo "xset s off" >> ~/.config/openbox/autostart
+echo "xset -dpms" >> ~/.config/openbox/autostart
+echo "cd ~/fotokiste/ ; python3 fotokiste.py | tee fotokiste.log" >> ~/.config/openbox/autostart
+
+# GUI-Boot with autologin
+sudo raspi-config nonint do_boot_behaviour B4
+
+echo "****************"
+echo "*  Syncing...  *"
+echo "****************"
+sudo sync
